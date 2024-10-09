e334a46822b46791c1f31c45016db2ccca90ae07
phanstudio <ajugapeterben@gmail.com>
2024-10-09 09:32:01 +0100

    update gitignore

--- addons/blockchains_sdk/blockchains_sdk.gd
+++ addons/blockchains_sdk/blockchains_sdk.gd
@@ -6,7 +6,7 @@ func _enter_tree():
 	add_autoload_singleton("BlockchainGlobal", "res://addons/blockchains_sdk/autoloads/blockchain_global.gd")
 	
 	# Custom types
-	add_custom_type("WalletUI", "Control", preload("res://addons/blockchains_sdk/ui/wallet_ui.gd"), preload("res://icon.svg"))
+	add_custom_type("WalletUI", "Control", preload("res://addons/blockchains_sdk/ui/wallet_ui.gd"), preload("res://logo.png"))
 
 func _exit_tree():
 	# Clean up
